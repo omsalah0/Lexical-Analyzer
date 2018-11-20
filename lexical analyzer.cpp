0a4c36f006e9a1bfce26f657d4004576e48e4db2
omar zakaria <39250810+omsalah0@users.noreply.github.com>
2018-11-20 21:44:19 +0200

    Add files via upload

--- lexical analyzer.cpp
+++ lexical analyzer.cpp
@@ -1,137 +1,151 @@
-#include<fstream>
-#include<string>
-#include<iostream>
-#include<ctype.h> //for testing and mapping characters
+    #include<fstream>
+    #include<string>
+    #include<iostream>
+    #include<ctype.h> //for testing and mapping characters
 
-using namespace std;
+    using namespace std;
 
-int isMultioperator(char source[]){
+    int isMultioperator(char source[]){
 
-    char separators[3][3] = {":=", "==", "!="};
+	   char separators[3][3] = {":=", "==", "!="};
 
-    bool gate = false;
+	   bool gate = false;
 
-    for(int i = 0 ; i < 3 ; i++){
+	   for(int i = 0 ; i < 3 ; i++){
     
-	   if( separators[i] == source){
+		  if( separators[i] == source){
 	   
-		  gate = true;
-		  break;
+			 gate = true;
+			 break;
 	   
+		  }
+    
 	   }
-    
+
+    return gate;
     }
 
-return gate;
-}
 
-
-int isKeyword(char source[]){
+    int isKeyword(char source[]){
     
-    char keyword[11][7] ={"WRITE","READ","IF","ELSE","RETURN","BEGIN","END","MAIN","STRING","INT","REAL"};
+	   char keyword[11][7] ={"WRITE","READ","IF","ELSE","RETURN","BEGIN","END","MAIN","STRING","INT","REAL"};
     
-    bool gate = false ;
+	   bool gate = false ;
 
-    for(int i =0 ;i<11;i++){
+	   for(int i =0 ;i<11;i++){
 	   
-	   if(strcmp(keyword[i] , source)==0){
-		  gate = true;
-		  break;
-	   }
+		  if(strcmp(keyword[i] , source)==0){
+			 gate = true;
+			 break;
+		  }
     
+	   }
+
+    return gate;
     }
-
-return gate;
-}
-
 
 int main(){
 
-    char chh, source[100], operators[] = "+-*/%=", separators[] = ";,()", digit[] = "123456789";
-int j = 0;
-ifstream fin("source code.txt");
+char chh, source[100], operators[] = "+-*/%=", separators[] = ";,()", digit[] = "0123456789",digits[100];
+    int j = 0, k = 0;
+    ifstream fin("source code.txt");
 
-if(!fin.is_open()){
+    if(!fin.is_open()){
 
-    cout<<"can not open this file \n";
+	   cout<<"can not open this file \n";
     
-    exit(0); //0:exit succesfull cleanup for terminating programs
-}
-
-while(!fin.eof()){
-    
-    chh = fin.get();
-
-
-    /*if(chh == ' '){
-    
-	   continue;
+	   exit(0); //0:exit succesfull cleanup for terminating programs
     }
-    */
-    for(int d = 0 ; d < 4 ; d++){
+
+    while(!fin.eof()){
+    
+	   chh = fin.get();
+
+
+	   /*if(chh == ' '){
+    
+		  continue;
+	   }
+	   */
+	   for(int d = 0 ; d < 4 ; d++){
 	  
-	   if(chh == separators[d])
+		  if(chh == separators[d])
 
-		  cout<<chh<<" is single separator\n";
+			 cout<<chh<<" is single separator\n";
     
     
-    }
+	   }
 
-     for(int q = 0 ; q < 9 ; q++){
+	   /* for(int q = 0 ; q < 9 ; q++){
 	  
-	   if(chh == digit[q])
+		  if(chh == digit[q])
 
-		  cout<<chh<<" is Digit\n";
+			 cout<<chh<<" is Digit\n";
     
     
-    }
+	   }*/
 
-    for(int i = 0 ; i < 6 ; i++){ //note for plus
+	   for(int i = 0 ; i < 6 ; i++){ //note for plus
     
-	   if(chh == operators[i])
+		  if(chh == operators[i])
 		  
-		  cout<<chh<<" is operator\n";
+			 cout<<chh<<" is operator\n";
     
-    }
-
-    if(isalnum(chh)){
+	   }
     
-	   source[j++] = chh;
-    }
-
-    else if((chh == ' ' || chh == '\n') && (j != 0)){
-    
-	   source[j] = '\0'; //null terminated string , in std::string not null terminated you make comiler to print as it be.
-
-	   j = 0;
-
-	   if(isKeyword(source) == true)
-		
-		  cout<<source<<" is keyword\n";
-
-	   else if(isMultioperator(source) == true)
+	   if(chh=='0' || chh=='1' ||chh=='2' || chh=='3' || chh=='4' || chh=='5' || chh=='6' || chh=='7' || chh=='8' || chh=='9'||chh==' '||chh==';'||chh=='\n'){
+		  if(chh=='0' || chh=='1' ||chh=='2' || chh=='3' || chh=='4' || chh=='5' || chh=='6' || chh=='7' || chh=='8' || chh=='9'){
+			 digits[k++] = chh;}
+		  if((chh==' '||chh==' \n'||chh==';')&&(k!=0)){
+			 digits[k] = '\0'; //null terminated 
+			 k = 0;
 		  
-		  cout<<source<<" is multiseparator\n";
+
+	   //cout<<digits<<" is digital\n";   
 	   
-	   else
-		  
-		  cout<<source<<" is identifier\n";
+		  }
     
-        //remainder separator function
+	   }
+	    if(isalnum(chh)){
+		   
+		  source[j++] = chh;
+	   }
+    
+		  else if((chh == ' ' || chh == '\n') && (j != 0)){
+    
+			 source[j] = '\0'; //null terminated string , in std::string not null terminated you make comiler to print as it be.
+
+			 j = 0;
+
+			 if(isKeyword(source) == true)
+		
+				 cout<<source<<" is keyword\n";
+
+			 else if(isMultioperator(source) == true)
+		  
+				cout<<source<<" is multiseparator\n";
+	   
+			 else if(source == digits){cout<<digits<<" is digits\n";}
+
+			 else
+				cout<<source<<" is identifier\n";
+    
+		  //remainder separator function
 	   
 	  
     
    
     
-    }
+	   }
     
 
-}
+    }
 
-fin.close();
+    fin.close();
 
-system("pause");
-return 0 ;
+    system("pause");
+    return 0 ;
 
 
-}
+    }
+    
